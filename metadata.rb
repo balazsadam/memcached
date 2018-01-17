b8a421c7b8eed8e29c01f2fc259c97c26999c04d
Balázs Ádám <balazsasd@balazsasd-wifi.hq.virgo.hu>
2018-01-17 12:02:53 +0100

    remove tmp dir true from systemd template

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs memcached and includes memcached_instance resource f
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '5.0.1'
 
-depends           'runit', '>= 1.2.0'
+depends           'runit', '>= 2.2.0'
 
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
